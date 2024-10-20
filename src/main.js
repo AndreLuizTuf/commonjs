abccf2e2fac61e0f057baea22bc03081ee44ae05
André Luiz <andre.luiz.a.f@hotmail.com>
2024-10-20 10:50:16 -0300

    Export Consts

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 const p = require("./services/products");
+const config = require("./services/config");
+
+
 async function main() {
   console.log("carrinho compras: ");
-  p.getFullName("408", "mousepad");
-  p.getProductLabel("mousepad");
+  
+//   p.getFullName("408", "mousepad");
+//   p.getProductLabel("mousepad");
+
+console.log(config.client);
 }
 
 main();
