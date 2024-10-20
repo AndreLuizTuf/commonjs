401637a2b7a36bc0e0c48962fa14a3cb93f1e940
André Luiz <andre.luiz.a.f@hotmail.com>
2024-10-20 10:36:30 -0300

    Multiple Exports

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-const p = require("./services/products")
+const p = require("./services/products");
 async function main() {
-    console.log("carrinho compras: ");
-    p.getFullName("408", "mousepad")
+  console.log("carrinho compras: ");
+  p.getFullName("408", "mousepad");
+  p.getProductLabel("mousepad");
 }
 
-main()
+main();
